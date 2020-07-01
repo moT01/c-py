04b350f7ad5f1d1feeb588bb827a2f0ad5cb955f
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2020-07-01 16:36:57 -0400

    30.1

--- tests/20_test.py
+++ tests/20_test.py
@@ -1,12 +1,12 @@
-import unittest
-import sys
-sys.path.append('..')
+# import unittest
+# import sys
+# sys.path.append('..')
 
-from blackjack import *
+# from blackjack import *
 
-class PythonTest(unittest.TestCase):
-    def test_python(self):
-        self.assertEqual(value, 10, "Variable named 'value' should be set to '10'")
+# class PythonTest(unittest.TestCase):
+#     def test_python(self):
+#         self.assertEqual(value, 10, "Variable named 'value' should be set to '10'")
 
-if __name__ == '__main__':
-    unittest.main() 
+# if __name__ == '__main__':
+#     unittest.main() 
